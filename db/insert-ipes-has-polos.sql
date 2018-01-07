cd67e1dcd4636b05684712fed8010e0f0126dd05
fedel@fedel.net.br <fedel@fedel.net.br>
2018-01-06 23:01:43 -0200

    Novos dados auxiliares

---
+++ db/insert-ipes-has-polos.sql
@@ -0,0 +1,6 @@
+insert into ipes_has_polos (ipes_id, polos_id) values (	12	,	81	);
+insert into ipes_has_polos (ipes_id, polos_id) values (	12	,	115	);
+insert into ipes_has_polos (ipes_id, polos_id) values (	12	,	116	);
+insert into ipes_has_polos (ipes_id, polos_id) values (	17	,	117	);
+insert into ipes_has_polos (ipes_id, polos_id) values (	17	,	118	);
+
---
+++ db/insert-ipes-incomp.sql
@@ -0,0 +1,3 @@
+insert into ipes (id, sigla, lat, lng, logradouro, bairro, cidade, estado, cep, telefone, url, url2) values (12,'CEFET-RJ',-22.91190060,-43.22433052,'Av. Maracanã,229','Maracanã','Rio De Janeiro','RJ','20271110','(21) 2549--666','http://uab.cefet-rj.br/','http://uab.cefet-rj.br/');
+insert into ipes (id, sigla, lat, lng, logradouro, bairro, cidade, estado, cep, telefone, url, url2) values (17,'FIOCRUZ',-22.87670000,-43.24460000,'','','Rio De Janeiro','RJ','21041210','(21) 2598-2996','www.ead.fiocruz.br','http://www.ead.fiocruz.br/');
+
